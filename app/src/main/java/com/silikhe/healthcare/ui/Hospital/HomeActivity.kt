5cbfb9d3f921159505d3cfd1711d3ac563b3b43c
silikhe <silaswan@gmail.com>
2021-10-21 01:00:18 +0300

    Added drawe navigation

--- app/src/main/java/com/silikhe/healthcare/ui/Hospital/HomeActivity.kt
+++ app/src/main/java/com/silikhe/healthcare/ui/Hospital/HomeActivity.kt
@@ -1,42 +1,118 @@
 package com.silikhe.healthcare.ui.Hospital
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.drawerlayout.widget.DrawerLayout.DrawerListener
+import com.google.android.material.navigation.NavigationView
+import java.security.AccessController.getContext
+import androidx.core.view.GravityCompat
+import android.widget.Toolbar
 import com.silikhe.healthcare.R
 
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+//    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.silikhe.healthcare.R.layout.activity_home)
 
+//        val toolbar: Toolbar = findViewById(R.id.toolbar)
+//        setSupportActionBar(toolbar)
 
         val fab: View = findViewById(R.id.hosDialog)
         fab.setOnClickListener { view ->
             val hospitalDialog = HospitalDialog()
-                hospitalDialog.show(supportFragmentManager, "Story Dialog")
+            hospitalDialog.show(supportFragmentManager, "Story Dialog")
+        }
+
+//        val navView: NavigationView = findViewById(R.id.)
+
+//        val drawer: DrawerLayout = findViewById(R.id.drawer_layout)
+//        val navigationView: NavigationView = findViewById(R.id.nav_view)
+//        val toggle = ActionBarDrawerToggle(
+//            this, drawer, R.string.open, R.string.close
+//        )
+//        drawer.addDrawerListener(toggle)
+//        toggle.syncState()
+//        navigationView.setNavigationItemSelectedListener(this)
+
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, R.string.open, R.string.close
+        )
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+        navigationView.setNavigationItemSelectedListener(this)
+
+        drawer.addDrawerListener(object : DrawerListener {
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+                //Log.i(TAG, "onDrawerSlide");
+            }
+
+            override fun onDrawerOpened(drawerView: View) {
+                Log.i(TAG, "onDrawerOpened")
+            }
+
+            override fun onDrawerClosed(drawerView: View) {
+                Log.i(TAG, "onDrawerClosed")
+            }
+
+            override fun onDrawerStateChanged(newState: Int) {
+                //Log.i(TAG, "onDrawerStateChanged");
+            }
+        })
+
+    }
+
+    override fun onBackPressed() {
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
 
-//    override fun onClick(view: View?) {
-//        when(view?.id){
-//            R.id.hosDialog->{
-//                val storyDialogue = HospitalDialog()
-//                storyDialogue.show(supportFragmentManager, "Story Dialog")
-//            }
-//        }
-//    }
-//    val fab = findViewById(R.id.hosDialog)
-//    fab.setOnClickListener { view ->
-//        Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-//            .setAction("Action", null)
-//            .show()
-//    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.nav_drawer_menu, menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+        return if (id == R.id.nav_gallery) {
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        val id = item.itemId
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_tools) {
+        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_send) {
+        }
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
 
 }
